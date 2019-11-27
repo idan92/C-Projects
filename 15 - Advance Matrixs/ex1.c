691a889ef03cdd24154e46286fc7bebf9b163f4e
Idan Cohen <idancohen1201@gmail.com>
2019-11-27 22:08:25 +0200

    תרגיל ראשון גמור

--- 15 - Advance Matrixs/ex1.c
+++ 15 - Advance Matrixs/ex1.c
@@ -1,6 +1,33 @@
 #include "PointersLibs.h"
 
 
+int MainSlantSum(int *ptr_mat, unsigned short rows, unsigned short cols)
+{
+    int sum = ZERO;
+    for ( ;rows; rows--)
+    {
+        sum += *ptr_mat;
+        ptr_mat += cols;
+    }
+
+    return (sum);
+}
+
+int SlantWithMaxSum(int *ptr_mat, unsigned short rows, unsigned short cols)
+{
+    int max = MainSlantSum(ptr_mat, rows, cols);
+    int temp;
+    unsigned short slats;
+    ptr_mat++;
+    for (slats = ONE; slats < cols; slats++)
+    {
+        temp = MainSlantSum(ptr_mat++, rows, cols);
+        max = (temp > max) ? temp : max;
+    }
+
+    return (max);
+}
+
 void main(void)
 {
 
