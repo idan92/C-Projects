5d71d90440404c4d5406d637a91d92737358223e
Idan Cohen <idancohen1201@gmail.com>
2019-11-27 18:46:47 +0200

    בשביל שמרלינג

--- 14 - Struct/ex2.c
+++ 14 - Struct/ex2.c
@@ -15,23 +15,39 @@ void AddBook(struct book *ptr_vec, unsigned short *vec_length)
 {
     struct book b;
     // I/O book
-    // Check I/O book
-    ptr_vec = realloc(ptr_vec, ++(*vec_length));
+    // Check I/O book - if I/O is ok = do realloc : out from fun
+    ptr_vec = realloc(ptr_vec, ++(*vec_length) * sizeof(struct book));
     *(ptr_vec + (*vec_length) - ONE) = b;
 }
 
-struct book * SearchByName(struct book *ptr_vec, unsigned short *vec_length, string *name)
+struct book * SearchByName(struct book *ptr_vec, unsigned short vec_length, string *name)
 {
-    while (!StringCompare((*(ptr_vec++)).name, *name));
+    while (!StringCompare((*(ptr_vec++)).name, *name) && vec_length--);
 
     return (--ptr_vec);
 }
 
-struct book * SearchByWriter(struct book *ptr_vec, unsigned short *vec_length, string *writer)
+struct book * SearchByWriter(struct book *ptr_vec, unsigned short vec_length, string *writer)
 {
-    while (!StringCompare((*(ptr_vec++)).wrtier, *writer));
+    while (!StringCompare((*(ptr_vec++)).wrtier, *writer) && vec_length--);
 
     return (--ptr_vec);
+}
+
+struct book * SearchByNameAndWriter(struct book *ptr_vec, unsigned short *vec_length, string *name, string *writer)
+{
+
+}
+
+struct book * TwentyCentury(struct book *ptr_vec, unsigned short vec_length, struct book *ptr_book_vec)
+{
+    ptr_book_vec = malloc(sizeof(struct book));
+    unsigned int length = ZERO;
+    unsigned short counter;
+    for (counter = ZERO; counter < vec_length; counter++)
+    {
+
+    }
 }
 
 void main(void)
