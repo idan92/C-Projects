54155e4ecc9a04fa86eeb8a9a1b820df036d7196
Idan Cohen <idancohen1201@gmail.com>
2019-11-27 17:45:54 +0200

    עד סעיף ג' כולל

--- 14 - Struct/ex2.c
+++ 14 - Struct/ex2.c
@@ -1,9 +1,38 @@
 #include "PointersLibs.h"
+#include <malloc.h>
 
 struct book
 {
-    gygy
+    string name;
+    string wrtier;
+    string company;
+    int year;
+    int price;
+    int id;
 };
+
+void AddBook(struct book *ptr_vec, unsigned short *vec_length)
+{
+    struct book b;
+    // I/O book
+    // Check I/O book
+    ptr_vec = realloc(ptr_vec, ++(*vec_length));
+    *(ptr_vec + (*vec_length) - ONE) = b;
+}
+
+struct book * SearchByName(struct book *ptr_vec, unsigned short *vec_length, string *name)
+{
+    while (!StringCompare((*(ptr_vec++)).name, *name));
+
+    return (--ptr_vec);
+}
+
+struct book * SearchByWriter(struct book *ptr_vec, unsigned short *vec_length, string *writer)
+{
+    while (!StringCompare((*(ptr_vec++)).wrtier, *writer));
+
+    return (--ptr_vec);
+}
 
 void main(void)
 {
