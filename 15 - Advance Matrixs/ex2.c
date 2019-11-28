5c1005fb1ef999451ea9ef849cf8e208ead92d6c
Idan Cohen <idancohen1201@gmail.com>
2019-11-28 08:55:20 +0200

    הוספת פוקנציה לבדיקת הסכום הכי גדול באלכסון

--- 15 - Advance Matrixs/ex2.c
+++ 15 - Advance Matrixs/ex2.c
@@ -1,6 +1,13 @@
 #include "PointersLibs.h"
 
+int MaxSumInSlant(int *ptr_mat, unsigned short rows, unsigned short cols)
+{
+    int max = ZERO;
+    for (; rows; rows--)
+    {
 
+    }
+}
 
 void main(void)
 {
