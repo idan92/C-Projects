9d195d76a6744325f80046e38364973ef4ffc341
Idan Cohen <idancohen1201@gmail.com>
2019-12-03 21:54:35 +0200

    Stack working

--- 16 - Stack/ex1.c
+++ 16 - Stack/ex1.c
@@ -6,16 +6,38 @@ struct stack
     unsigned int items;
 };
 
+void CreateStack(struct stack * sk)
+{
+    (*sk).items = ZERO;
+    (*sk).values = malloc(((*sk).items + ONE) * sizeof(*((*sk).values)));
+}
+
 void PushStack(struct stack * sk , void * item)
 {
-    (*sk).values = realloc((*sk).values, (++((*sk).items) * sizeof(*item)));
+    (*sk).values = realloc((*sk).values, ((++((*sk).items) + ONE) * sizeof(item)));
     *((*sk).values + (*sk).items) = item;
 }
 
 void * PopStack(struct stack * sk)
 {
-    void * value = *((*sk).values + (*sk).items);
-    (*sk).values = realloc((*sk).values, (--((*sk).items) * sizeof(*value)));
+    void * item = NULL;
+    if ((*sk).items)
+    {
+        item = *((*sk).values + (*sk).items);
+        (*sk).values = realloc((*sk).values, ((--((*sk).items) + ONE) * sizeof(item)));
+    }
 
-    return (value);
+    return (item);
+}
+
+void main(void)
+{
+    struct stack s;
+    CreateStack(&s);
+    PushStack(&s, 4);
+    PushStack(&s, 5);
+    PushStack(&s, 6);
+    printf("%hu\n", PopStack(&s));
+    printf("%hu\n", PopStack(&s));
+    printf("%hu\n", PopStack(&s));
 }
