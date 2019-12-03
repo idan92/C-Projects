7920b063111773220d6c9e9325cf5450fe9879ce
Idan Cohen <idancohen1201@gmail.com>
2019-12-03 22:58:23 +0200

    לסיים פונקציות

--- 16 - Stack/ex1.c
+++ 16 - Stack/ex1.c
@@ -12,6 +12,11 @@ void CreateStack(struct stack * sk)
     (*sk).values = malloc(((*sk).items + ONE) * sizeof(*((*sk).values)));
 }
 
+BOOLEAN IsEmptyStack(struct stack * sk)
+{
+    return (!(*sk).items);
+}
+
 void PushStack(struct stack * sk , void * item)
 {
     (*sk).values = realloc((*sk).values, ((++((*sk).items) + ONE) * sizeof(item)));
@@ -21,13 +26,66 @@ void PushStack(struct stack * sk , void * item)
 void * PopStack(struct stack * sk)
 {
     void * item = NULL;
-    if ((*sk).items)
+    if (!IsEmptyStack(sk))
     {
         item = *((*sk).values + (*sk).items);
         (*sk).values = realloc((*sk).values, ((--((*sk).items) + ONE) * sizeof(item)));
     }
 
     return (item);
+}
+
+void EmptyStack(struct stack * sk)
+{
+    while (!IsEmptyStack(sk))
+    {
+        PopStack(sk);
+    }
+}
+
+void CopyStack(struct stack * sk, struct stack * copy)
+{
+    struct stack temp1_sk;
+    void * item;
+    CreateStack(&temp1_sk);
+
+    while (!IsEmptyStack(sk))
+    {
+        PushStack(&temp1_sk, PopStack(sk));
+    }
+    while (!IsEmptyStack(&temp1_sk))
+    {
+        item = PopStack(&temp1_sk);
+        PushStack(copy, item);
+        PushStack(sk, item);
+    }
+}
+
+void OppositeStack(struct stack * sk)
+{
+    struct stack temp_sk;
+    CreateStack(&temp_sk);
+    CopyStack(sk, &temp_sk);
+    EmptyStack(sk);
+    while (!IsEmptyStack(&temp_sk))
+    {
+        PushStack(sk, PopStack(&temp_sk));
+    }
+}
+
+BOOLEAN IsEqualsStack(struct stack * sk1, struct stack * sk2)
+{
+    BOOLEAN flag = TRUE;
+    void * item_sk1;
+    void * item_sk2;
+    struct stack temp_sk;
+    CreateStack(&temp_sk);
+    while (!IsEmptyStack(sk1) && !IsEmptyStack(sk2) && flag)
+    {
+        item_sk1 = PopStack(sk1);
+        item_sk2 = PopStack(sk2);
+        // flag = (*item_sk1 == *item_sk2); //BlackBox
+    }
 }
 
 void main(void)
