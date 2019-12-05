7dc6899c20ada428f3567f09efb42d23b7875178
Idan Cohen <idancohen1201@gmail.com>
2019-12-05 17:20:19 +0200

    תרגיל אחד עובד

--- 16 - Stack/ex1.c
+++ 16 - Stack/ex1.c
@@ -1,5 +1,35 @@
 #include "Lib.h"
 
+int SectionOne(stack * sk)
+{
+    stack temp_sk;
+    InitializeStack(&temp_sk);
+    CopyStack(&temp_sk, sk);
+    int last_number = PopStack(&temp_sk).int_;
+    int this_number = PopStack(&temp_sk).int_;
+    while (this_number >= last_number)
+    {
+        last_number = this_number;
+        this_number = PopStack(&temp_sk).int_;
+    }
+
+    return (this_number);
+}
+
+int SectionOne(stack * sk)
+{
+    stack temp_sk;
+    InitializeStack(&temp_sk);
+    CopyStack(&temp_sk, sk);
+    unsigned int counter = ZERO;
+    int number = SectionOne(&temp_sk);
+    while (PopStack(&temp_sk).int_ != number)
+    {
+        counter++;
+    }
+
+    return (counter);
+}
 
 void main(void)
 {
@@ -9,8 +39,4 @@ void main(void)
     PushStack(&s, num1);
     PushStack(&s, num2);
     PushStack(&s, num3);
-    printf("%hu\n", SumStack(&s));
-    printf("%hu\n", PopStack(&s).int_);
-    printf("%hu\n", PopStack(&s).int_);
-    printf("%hu\n", PopStack(&s).int_);
 }
